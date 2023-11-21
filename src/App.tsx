b01bb36130aff63f26b216ba2e1dbe89c89bdbc7
Guilherme Calabria Lopes <guicalabriadev@gmail.com>
2023-11-21 17:24:05 +0100

    Use store and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-
+import { Box } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import { PrivateOutlet } from './utils/PrivateOutlet';
+import HomePage from './pages/HomePage';
+import ResultsPage from './pages/ResultsPage';
+import MainLayout from './layout/MainLayout';
 
 function App() {
-
   return (
-    <div>TODO</div>
-  )
+    <Box
+      sx={{
+        display: 'block',
+        height: '100vh',
+        width: '100vw',
+      }}
+    >
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="*" element={<PrivateOutlet />}>
+          <Route element={<MainLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path="results" element={<ResultsPage />} />
+          </Route>
+        </Route>
+      </Routes>
+    </Box>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import { store } from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
-)
+);
