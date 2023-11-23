9cda60f2f848ec4cc6f1d39ad4f6d6d964d3bb8a
Guilherme Calabria Lopes <guicalabriadev@gmail.com>
2023-11-23 13:53:23 +0100

    Add Configs model

--- src/redux/api/configsApi.ts
+++ src/redux/api/configsApi.ts
@@ -1,13 +1,21 @@
 import { baseApi } from './api';
 
-export interface User {
-  id: number;
-  email: string;
+export interface Configs {
+  db_path: string;
+  db_table_name: string;
+  db_content_attribute_name: string;
+  index_path: string;
+  allowed_search_modes: {
+    default: boolean;
+    separated: boolean;
+    url: boolean;
+    file: boolean;
+  };
 }
 
 export const configsApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
-    fetchConfigs: builder.query<User, void>({
+    fetchConfigs: builder.query<Configs, void>({
       query: () => ({
         url: '/configs',
         method: 'GET',
