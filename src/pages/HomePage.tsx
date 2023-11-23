dffde0a6b63847ae2d89c74fdf02f0284ef4a81a
Guilherme Calabria Lopes <guicalabriadev@gmail.com>
2023-11-23 13:52:30 +0100

    Add code for debugging

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,25 +1,40 @@
 import { LoadingButton } from '@mui/lab';
-import { useFetchConfigsMutation } from '../api/authApi';
+import { useLazyFetchConfigsQuery } from '../redux/api/configsApi';
+import { Box } from '@mui/material';
 
 function HomePage() {
-  const [fetchConfigs, { isLoading: fetchingConfigs }] =
-    useFetchConfigsMutation();
+  const [
+    fetchConfigs,
+    { data: configs, isFetching, isLoading, isError, isSuccess },
+  ] = useLazyFetchConfigsQuery();
 
-  const handleFetchConfigs = async () => {
-    try {
-      const configs = await fetchConfigs();
-      console.log(configs);
-    } catch (err) {
-      console.error(err);
-    }
+  const handleFetchConfigs = () => {
+    fetchConfigs();
   };
 
   return (
-    <div>
-      <LoadingButton onClick={handleFetchConfigs} loading={fetchingConfigs}>
+    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 4 }}>
+      <LoadingButton onClick={handleFetchConfigs} loading={isLoading}>
         Fetch
       </LoadingButton>
-    </div>
+
+      <Box>
+        <p>isFetching: {isFetching}</p>
+        <p>isLoading: {isLoading}</p>
+        <p>isError: {isError}</p>
+        <p>isSuccess: {isSuccess}</p>
+      </Box>
+      {configs && (
+        <Box>
+          <p>db_path: {configs.db_path}</p>
+          <p>db_content_attribute_name: {configs.db_content_attribute_name}</p>
+          <p>db_table_name: {configs.db_table_name}</p>
+          <p>
+            allowed_search_modes: {JSON.stringify(configs.allowed_search_modes)}
+          </p>
+        </Box>
+      )}
+    </Box>
   );
 }
 
