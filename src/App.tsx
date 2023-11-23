9d0478aaec100e07b019ee8b3e1dcd642abacebb
Guilherme Calabria Lopes <guicalabriadev@gmail.com>
2023-11-23 16:08:18 +0100

    Add PersistLogin

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Box } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
-import { PrivateOutlet } from './utils/PrivateOutlet';
 import HomePage from './pages/HomePage';
 import ResultsPage from './pages/ResultsPage';
 import MainLayout from './layout/MainLayout';
 import LandingPage from './pages/LandingPage';
 import RegistrationPage from './pages/RegistrationPage';
+import PersistLogin from './utils/PersistLogin';
 
 function App() {
   return (
@@ -21,12 +21,15 @@ function App() {
         <Route index element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegistrationPage />} />
-        <Route path="*" element={<PrivateOutlet />}>
+
+        {/* Protected Routes */}
+        <Route element={<PersistLogin />}>
           <Route element={<MainLayout />}>
             <Route path="home" element={<HomePage />} />
             <Route path="results" element={<ResultsPage />} />
           </Route>
         </Route>
+        {/* End Protected Routes */}
       </Routes>
     </Box>
   );
---
+++ src/utils/PersistLogin.tsx
@@ -0,0 +1,108 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Outlet } from 'react-router-dom';
+import { useEffect, useRef, useState } from 'react';
+import usePersist from '../hooks/usePersist';
+import { Box, CircularProgress, Paper, Typography } from '@mui/material';
+import { useRefreshTokensMutation } from '../redux/api/authApi';
+import { useSelector } from 'react-redux';
+import { selectCurrentToken } from '../redux/slices/authSlice';
+
+const PersistLogin = () => {
+  const [persist] = usePersist();
+  const token = useSelector(selectCurrentToken);
+  const effectRan = useRef(false); // for react 18 strict mode
+
+  const [trueSuccess, setTrueSuccess] = useState(false);
+
+  const [refresh, { isUninitialized, isLoading, isSuccess, isError, error }] =
+    useRefreshTokensMutation();
+
+  useEffect(() => {
+    // The following code is to handle react 18 Strict Mode
+    if (effectRan.current === true || process.env.NODE_ENV !== 'development') {
+      const verifyRefreshToken = async () => {
+        try {
+          await refresh(localStorage.getItem('accessToken')!);
+          setTrueSuccess(true);
+        } catch (err) {
+          console.error(err);
+        }
+      };
+
+      if (!token && persist) {
+        verifyRefreshToken();
+      }
+    }
+
+    return () => {
+      effectRan.current = true;
+    };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  let content;
+  if (!persist) {
+    // persist: no
+    console.log('no persist');
+    return <Outlet />;
+  } else if (isLoading) {
+    //persist: yes, token: no
+    console.log('loading');
+    content = <CircularProgress />;
+  } else if (isError) {
+    //persist: yes, token: no
+    content = (
+      <Paper
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 4,
+          p: 4,
+          textAlign: 'center',
+          width: 400, // TODO: check if there a better way of defining the width
+        }}
+        elevation={3}
+      >
+        <Typography variant="h3" component="h3">
+          Oops! Something went wrong
+        </Typography>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            gap: 2,
+          }}
+        >
+          <Typography variant="body1" component="p">
+            Try logging in again.
+          </Typography>
+        </Box>
+      </Paper>
+    );
+  } else if (isSuccess && trueSuccess) {
+    //persist: yes, token: yes
+    console.log('success');
+    return <Outlet />;
+  } else if (token && isUninitialized) {
+    //persist: yes, token: yes
+    return <Outlet />;
+  }
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      {content}
+    </Box>
+  );
+};
+export default PersistLogin;
