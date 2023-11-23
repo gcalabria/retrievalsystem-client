6c5d5ad604f34803539581f67e7c524bb481aa03
Guilherme Calabria Lopes <guicalabriadev@gmail.com>
2023-11-23 19:35:21 +0100

    Clean code

--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -17,40 +17,6 @@ const baseQuery = fetchBaseQuery({
 
 const baseQueryWithRetry = retry(baseQuery, { maxRetries: 6 });
 
-// TODO: consider implementing this
-// const baseQueryWithReauth = async (args, api, extraOptions) => {
-//   // console.log(args) // request url, method, body
-//   // console.log(api) // signal, dispatch, getState()
-//   // console.log(extraOptions) //custom like {shout: true}
-
-//   let result = await baseQuery(args, api, extraOptions)
-
-//   // If you want, handle other status codes, too
-//   if (result?.error?.status === 403) {
-//       console.log('sending refresh token')
-
-//       // send refresh token to get new access token
-//       const refreshResult = await baseQuery('/auth/refresh', api, extraOptions)
-
-//       if (refreshResult?.data) {
-
-//           // store the new token
-//           api.dispatch(setCredentials({ ...refreshResult.data }))
-
-//           // retry original query with new access token
-//           result = await baseQuery(args, api, extraOptions)
-//       } else {
-
-//           if (refreshResult?.error?.status === 403) {
-//               refreshResult.error.data.message = "Your login has expired."
-//           }
-//           return refreshResult
-//       }
-//   }
-
-//   return result
-// }
-
 export const baseApi = createApi({
   reducerPath: 'baseApi',
   baseQuery: baseQueryWithRetry,
@@ -58,9 +24,3 @@ export const baseApi = createApi({
 
   endpoints: () => ({}),
 });
-
-// export const enhancedApi = api.enhanceEndpoints({
-//   endpoints: () => ({
-//     getPost: () => 'test',
-//   }),
-// })
