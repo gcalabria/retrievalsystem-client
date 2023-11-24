31690143b8584f2825341032b16da793f588e1a3
Guilherme Calabria Lopes <guicalabriadev@gmail.com>
2023-11-24 15:04:10 +0100

    Add support for sass

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import './index.scss';
 import { store } from './redux/store.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
