861ffb883fdee48b9e6e3b0bf08e1b1faf09245c
Guilherme Calabria Lopes <guicalabriadev@gmail.com>
2023-11-24 10:26:30 +0100

    Add SearchPage

---
+++ src/pages/SearchPage/SearchPage.tsx
@@ -0,0 +1,38 @@
+/// <reference types="vite-plugin-svgr/client" /> // For SVG imports
+
+import { Box, Stack, Typography } from '@mui/material';
+import SelmaLogo from '../../assets/SELMA-Logo_code.svg?react';
+import { useTheme } from '@mui/material/styles';
+
+function SearchPage() {
+  const theme = useTheme();
+  const logoTheme = theme.palette.mode === 'light' ? 'dark-logo' : 'light-logo';
+
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 4 }}>
+      <Stack
+        direction="row"
+        sx={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          mb: 6,
+        }}
+      >
+        <SelmaLogo className={logoTheme} height="12rem" width="12rem" />
+        <Typography
+          variant="h2"
+          sx={{
+            color: 'text.primary',
+            fontWeight: 800,
+          }}
+        >
+          SELMA
+        </Typography>
+      </Stack>
+
+      {/* <SearchForm /> */}
+    </Box>
+  );
+}
+
+export default SearchPage;
