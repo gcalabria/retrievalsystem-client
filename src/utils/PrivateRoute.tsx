ac11dc23571377cbc5c69ce723503afac203dd59
Guilherme Calabria Lopes <guicalabriadev@gmail.com>
2023-11-27 16:53:57 +0100

    Refactor PrivateRoute to fe fetch user when loading

--- src/utils/PrivateRoute.tsx
+++ src/utils/PrivateRoute.tsx
@@ -1,5 +1,6 @@
 import { useLocation, Navigate } from 'react-router-dom';
-import { useUser } from './UserProvider';
+import { userApi } from '../redux/api/userApi';
+import { Box, CircularProgress } from '@mui/material';
 
 export default function PrivateRoute({
   children,
@@ -7,14 +8,40 @@ export default function PrivateRoute({
   children: React.ReactNode;
 }) {
   const location = useLocation();
-  const user = useUser();
 
-  if (user === undefined) {
-    return null;
-  } else if (user) {
-    return children;
-  } else {
-    const url = location.pathname + location.search + location.hash;
-    return <Navigate to="/login" state={{ next: url }} />;
+  const { isLoading, isFetching } = userApi.endpoints.fetchUser.useQuery(
+    undefined,
+    {
+      skip: false,
+      refetchOnMountOrArgChange: true,
+    },
+  );
+
+  const loading = isLoading || isFetching;
+
+  const user = userApi.endpoints.fetchUser.useQueryState(undefined, {
+    selectFromResult: ({ data }) => data!,
+  });
+
+  if (loading) {
+    return (
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100%',
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    );
   }
+
+  if (user) {
+    return children;
+  }
+
+  const url = location.pathname + location.search + location.hash;
+  return <Navigate to="/login" state={{ next: url }} />;
 }
