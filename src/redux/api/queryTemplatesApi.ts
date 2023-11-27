bcb72c8037dd9799bcc9d40b77d5a1254875b9e9
Guilherme Calabria Lopes <guicalabriadev@gmail.com>
2023-11-27 17:56:51 +0100

    Add tags to query template api

--- src/redux/api/queryTemplatesApi.ts
+++ src/redux/api/queryTemplatesApi.ts
@@ -69,6 +69,7 @@ export const queryTemplatesApi = baseApi.injectEndpoints({
         url: `/users/${userId}/query-templates?offset=${pagination.offset}`,
         method: 'GET',
       }),
+      providesTags: ['QueryTemplate'],
     }),
     deleteQueryTemplate: builder.mutation<IDeleteQueryTemplateResponse, number>(
       {
@@ -76,6 +77,7 @@ export const queryTemplatesApi = baseApi.injectEndpoints({
           url: `/query-templates/${id}`,
           method: 'DELETE',
         }),
+        invalidatesTags: ['QueryTemplate'],
       },
     ),
   }),
