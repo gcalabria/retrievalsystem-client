37c6a195debf57174702e89c421740b22af40db4
Guilherme Calabria Lopes <guicalabriadev@gmail.com>
2023-11-27 17:26:52 +0100

    Add comments to explain cases in PublicRoute

--- src/utils/PublicRoute.tsx
+++ src/utils/PublicRoute.tsx
@@ -10,10 +10,17 @@ export default function PublicRoute({
   const user = useSelector(selectCurrentUser);
 
   if (user === undefined) {
+    // If user is undefined, then the app is not ready to provide user information yet.
+    // Thus, we return null to prevent the app from rendering anything.
+    // As soon as the user var is set to null or an object, we can render the app.
     return null;
   } else if (user) {
+    // Here the app tried to get the user information and succeed.
+    // Thus, the public route should not be accessible.
     return <Navigate to="/" />;
   } else {
+    // Here the app tried to get the user information but failed.
+    // Thus, the public route should be accessible.
     return children;
   }
 }
